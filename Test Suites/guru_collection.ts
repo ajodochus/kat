c8e246bd31d10482ad23a65b2d80a26844158304
STPAG\Bernd.Wissmeier <bernd.wissmeier@stp-online.de>
2018-06-15 22:22:15 +0200

    testguru

---
+++ Test Suites/guru_collection.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>guru_collection</name>
+   <tag></tag>
+   <executionMode>SEQUENTIAL</executionMode>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <runConfigurationId>Edge</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/testguru</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
+</TestSuiteCollectionEntity>
--- Test Suites/testguru.ts
+++ Test Suites/testguru.ts
@@ -4,7 +4,7 @@
    <name>testguru</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-06-14T23:12:14</lastRun>
+   <lastRun>2018-06-15T22:09:44</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
